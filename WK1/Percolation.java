c2bb995ed94e17233c17955b41a83f8c0b43bb70
John Pang <jwp36@glink.drexel.edu>
2015-09-07 22:45:19 -0400

    Cleaned up Percolation.java and added comments.
    Backwash bug needs to be fixed.

--- WK1/Percolation.java
+++ WK1/Percolation.java
@@ -1,5 +1,9 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * Any methods with parameters i and j expect a value between 1 and N,
+ * where (1, 1) is the upper left site.
+ */
 public class Percolation
 {
     private WeightedQuickUnionUF sites;
@@ -8,11 +12,6 @@ public class Percolation
     private int N;
     private int virtualTopSiteIndex;
     private int virtualBottomSiteIndex;
-
-
-    //Note: i and j are integers between 1 and N, where 1, 1 is the upper
-    //left. Throw java.lang.IndexOutOfBoundsException if any argument is
-    //out of range.
 
     //Creates N by N grid with all sites blocked
     public Percolation(int N)
@@ -23,70 +22,55 @@ public class Percolation
         }
 
         this.N = N;
-        virtualTopSiteIndex = N * N;
-        virtualBottomSiteIndex = N * N + 1;
-
-        //Add 2 sites (last 2 indices) to determine if we percolate
-        //Indices from 0 to N-1 correspond to the real sites
-        //Index N corresponds to the top fake site
-        //Index N + 1 corresponds to the bottom fake site 
         int numSites = N * N; 
+        
+        //Indices 0 to N-1 correspond to non-virtual sites.
+        virtualTopSiteIndex = numSites;
+        virtualBottomSiteIndex = numSites + 1;
 
+        //Add two virtual sites used to determine if the system percolates.
         sites = new WeightedQuickUnionUF(numSites + 2);
         siteStates = new boolean[numSites];
-
-        //siteStates[virtualTopSiteIndex] = true;
-        //siteStates[virtualBottomSiteIndex] = true;
     }
 
     //Open a site. If open already, np.
     public void open(int i, int j)
     {
-        //must validate i and j here. 
-
         if (isOpen(i, j))
         {
             return;
         }
 
+        //Open the current site.
         int siteIndex = 2Dto1D(i, j);
         siteStates[siteIndex] = true;
 
-        //Connect to the adjacent site on top of the current site.
-        //If top row, connect to the faux top site.
+        //Connect the current site to the adjacent top site.
+        //If the current site is on the top row, connect to the virtual top site.
         if (i == 1)
         {
             sites.union(siteIndex, virtualTopSiteIndex);
         }
-        else
+        else if (isOpen(i - 1, j))
         {
-            if (isOpen(i - 1, j))
-            {
-                int adjacentTopSiteIndex = 2Dto1D(i - 1, j);
-                sites.union(siteIndex, adjacentTopSiteIndex);
-            }
+            int adjacentTopSiteIndex = 2Dto1D(i - 1, j);
+            sites.union(siteIndex, adjacentTopSiteIndex);
         }
 
-        //Connect to the adjacent site on the left of the current site.
-        //If the current site is located in the leftmost column, do nothing.
-        if (j != 1)
-        {
-            if (isOpen(i, j - 1))
-            {
-                int adjacentLeftSiteIndex = 2Dto1D(i, j - 1);
-                sites.union(siteIndex, adjacentLeftSiteIndex);
-            }
+        //Connect the current site to the adjacent left site.
+        //If the current site is on the leftmost column, do nothing.
+        if ((j != 1) && isOpen(i, j - 1))
+        { 
+            int adjacentLeftSiteIndex = 2Dto1D(i, j - 1);
+            sites.union(siteIndex, adjacentLeftSiteIndex);
         }
 
-        //Connect to the adjacent site on the right of the current site.
-        //If the current site is located in the rightmost column, do nothing. 
-        if (j != N)
+        //Connect the current site to the adjacent right site.
+        //If the current site is on the rightmost column, do nothing. 
+        if ((j != N) && isOpen(i, j + 1))
         {
-            if (isOpen(i, j + 1))
-            {
-                int adjacentRightSiteIndex = 2Dto1D(i, j + 1);
-                sites.union(siteIndex, adjacentRightSiteIndex);
-            }
+            int adjacentRightSiteIndex = 2Dto1D(i, j + 1);
+            sites.union(siteIndex, adjacentRightSiteIndex);
         }
 
         //Connect to the adjacent site on bottom of the current site.
@@ -95,23 +79,26 @@ public class Percolation
         {
             sites.union(siteIndex, virtualBottomSiteIndex);
         }
-        else
+        else if (isOpen(i + 1, j))
         {
-            if (isOpen(i + 1, j))
-            {
-                int adjacentBottomSiteIndex = 2Dto1D(i + 1, j);
-                sites.union(siteIndex, adjacentBottomSiteIndex);
-            }
+            int adjacentBottomSiteIndex = 2Dto1D(i + 1, j);
+            sites.union(siteIndex, adjacentBottomSiteIndex);
         }
     }
 
+    /**
+     * Determine if a site is open or not.
+     */
     public boolean isOpen(int i, int j)
     {
         int siteIndex = 2Dto1D(i, j);
         return siteStates[siteIndex];
     }
 
-    //Determines if a site is full
+    /**
+     * Determine if a site is full or not.
+     * A full site is a site that is connected to a site on the top row.
+     */
     public boolean isFull(int i, int j)
     {
         if (!isOpen(i, j))
@@ -124,23 +111,38 @@ public class Percolation
         return sites.connected(siteIndex, virtualTopSiteIndex);
     }
 
-    //Checks to see if the system percolates
+    /**
+     * Determine if the system percolates.
+     */
     public boolean percolates()
     {
         return sites.connected(virtualTopSiteIndex, virtualBottomSiteIndex);
     }
 
+
+
+    /**
+     * Converts 2D coordinates, i and j, to a one dimensional index.
+     */
     private int 2Dto1D(int i, int j)
     {
-        if ((i < 1) || (i > N) || (j < 1) || (j > N))
-        {
-            throw new IndexOutOfBoundsException();
-        }
-
+        validate2D(i, j)
         return (i - 1) * N + (j - 1);
     }
 
-    public static void main(String[] args)
+    /**
+     * Validate 2D coordinates, i and j.
+     */
+    private void validate2D(int i, int j)
     {
+        if ((i < 1) || (i > N))
+        {
+            throw new IndexOutOfBoundsException("Row index i is out of range.");
+        }
+        
+        if ((j < 1) || (j > N))
+        {
+            throw new IndexOutOfBoundsException("Column index j is out of range.");
+        }
     }
  }
